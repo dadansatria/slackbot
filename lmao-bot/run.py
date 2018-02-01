a59fecd1f44f86092e7bece480016a79552f8116
Dadan Satria <dadansatria7@gmail.com>
2018-02-01 23:47:33 +0700

    command

--- lmao-bot/run.py
+++ lmao-bot/run.py
@@ -5,6 +5,20 @@ import logging.config
 
 from slackbot import settings
 from slackbot.bot import Bot
+from slackbot.bot import respond_to
+from slackbot.bot import listen_to
+import re
+
+
+@respond_to('hi', re.IGNORECASE)
+def hi(message):
+    message.reply('Opo?')
+    # react with thumb up emoji
+    message.react('+1')
+
+def test_bot_default_reply(driver):
+    driver.send_direct_message('youdontunderstandthiscommand do you')
+    driver.wait_for_bot_direct_message('.*You can ask me.*')
 
 
 def main():
